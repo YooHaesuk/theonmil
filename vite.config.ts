22c1a6acdc1cfc492d51b70af775e9e819893ab4
YooHaesuk <yhs85844@gmail.com>
2026-01-19 01:21:37 +0900

    npx wrangler pages deploy dist/public

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,10 @@ export default defineConfig({
     },
   },
   root: path.resolve(__dirname, "client"),
+  publicDir: path.resolve(__dirname, "client", "public"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    copyPublicDir: true,
   },
 });
